1df742b5a646ca84a457d8a5e9822d46e4628fde
Mitesh Prajapati <193383817+pr0je@users.noreply.github.com>
2026-09-11 11:43:18 +0530

    Update the following are
    1. Data Overview
    2. Datatype correction & feature engineering

--- main.py
+++ main.py
@@ -21,3 +21,40 @@ warnings.filterwarnings('ignore')
 # Load the dataset
 df = pd.read_csv('/content/bike_sharing.csv')
 df
+
+# Data Overview
+#Shape of the dataset
+print('Number of rows: ', df.shape[0])
+print('Number of columns: ', df.shape[1])
+
+# Data types of all attributes
+df.info()
+
+# Data type correction & Feature Engineering
+
+# Conver datetime column to proper datetime dtype.
+df['datetime'] = pd.to_datetime(df['datetime'])
+
+# Extract useful time-based features for EDA
+df['year'] = df['datetime'].dt.year
+df['month'] = df['datetime'].dt.month
+df['day'] = df['datetime'].dt.day
+df['hour'] = df['datetime'].dt.hour
+df['dayofweek'] = df['datetime'].dt.day_name()
+
+# Human-readable leabels for seasons & weather (used only for plotting/ readability)
+season_map = {1: 'spring', 2: 'summer', 3: 'fall', 4: 'winter'}
+weather_map = {1: 'clear', 2: 'Mist + Cloudly', 3: 'Light Snow/Rain', 4: 'Heavy Rain/Snow'}
+df['season_label'] = df['season'].map(season_map)
+df['weather_label'] = df['weather'].map(weather_map)
+
+# Convert nominal/ordinal-coded integer columns to 'category' dtype
+categorical_cols = ['season', 'holiday', 'workingday', 'weather']
+for col in categorical_cols:
+  df[col] = df[col].astype('category')
+
+numerical_cols = ['temp', 'atemp', 'humidity', 'windspeed', 'casual', 'registered', 'count']
+
+print('Categorical Columns:', categorical_cols)
+print('Numerical Columns:', numerical_cols)
+df.dtypes
